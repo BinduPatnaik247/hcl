d536862d8eb3f6f79b013e118f791cf4fd258b2b
BinduPatnaik247 <84894669+BinduPatnaik247@users.noreply.github.com>
2021-06-01 12:29:05 +0530

    Update RestaurantSpringbootApplication.java

--- Restaurant-hibernate/src/main/java/com/example/demo/RestaurantSpringbootApplication.java
+++ Restaurant-hibernate/src/main/java/com/example/demo/RestaurantSpringbootApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantSpringbootApplication {
+public class RestaurantSpringbootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantSpringbootApplication.class, args);
 	}
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
+		return application.sources(RestaurantSpringbootApplication.class); }
 
 }
